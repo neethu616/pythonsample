da7d00205356051a83299b5a8c24dd5e85f049cd
neethu616 <106176260+neethu616@users.noreply.github.com>
2022-08-26 10:57:51 +0530

    Update api.py

--- api.py
+++ api.py
@@ -116,4 +116,4 @@ api.add_resource(BookList, "/books")
 api.add_resource(Book, "/books/<int:id>")
 
 if __name__ == "__main__":
-    app.run(host="localhost", port=8000)
+    app.run(host="localhost", port=8080)
